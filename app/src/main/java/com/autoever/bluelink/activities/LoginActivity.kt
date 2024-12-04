07e41004ed13bd4b76eb254d821098afd19d6fad
zoonmy <pjm2571@naver.com>
2024-12-04 11:54:23 +0900

    refactor: 로그인 페이지 수정

--- app/src/main/java/com/autoever/bluelink/activities/LoginActivity.kt
+++ app/src/main/java/com/autoever/bluelink/activities/LoginActivity.kt
@@ -1,11 +1,16 @@
 package com.autoever.bluelink.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.autoever.bluelink.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +18,37 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        setUI()
+    }
+
+    fun setUI() {
+        val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
+        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+
+        // "로그인 버튼"
+        val textViewContinue = findViewById<TextView>(R.id.textViewContinue)
+        textViewContinue.setOnClickListener {
+            login(editTextEmail.text.toString(), editTextPassword.text.toString())
+        }
+    }
+
+    // 로그인 함수
+    private fun login(email: String, password: String) {
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                } else {
+                    // 로그인 실패
+                    Toast.makeText(
+                        baseContext,
+                        "로그인 실패: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
