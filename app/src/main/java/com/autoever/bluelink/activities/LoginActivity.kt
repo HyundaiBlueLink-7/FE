ade72e48498d8076a046951cf8f9b7dbf25028c4
zoonmy <pjm2571@naver.com>
2024-12-04 11:43:59 +0900

    feat: 회원가입 페이지 구현

---
+++ app/src/main/java/com/autoever/bluelink/activities/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.autoever.bluelink.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.autoever.bluelink.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+    }
+}
